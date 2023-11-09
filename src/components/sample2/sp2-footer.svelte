e0c8528e76873172896107c3bc54a1b8651d7bac
kimchan122 <chanchankim122@gmail.com>
2023-11-09 08:28:43 +0700

    first commit

---
+++ src/components/sample2/sp2-footer.svelte
@@ -0,0 +1,103 @@
+<script>
+    import { onMount } from "svelte";
+    import { reveal } from "svelte-reveal";
+</script>
+
+<footer id="footer" class="bg-darkgray">
+    <div class="footer">
+        <img
+            class="footer-logo"
+            src="images/MORI_HORIZONTAL_WHITE.png"
+            alt=""
+        />
+        <div>COPYRIGHT © 2023 MORI INC. ALL RIGHTS RESERVED</div>
+    </div>
+</footer>
+
+<style>
+    .bg-gray {
+        background-color: #eeeeee;
+        margin: 0 auto;
+    }
+    .bg-darkgray {
+        background-color: #1f1f1f;
+        margin: 0 auto;
+        display: flex;
+        justify-content: center;
+    }
+    .container {
+        padding-top: 70px;
+        width: 100%;
+        /* height: calc(100vh - 70px); */
+        max-width: 980px;
+        margin: 0 auto;
+        padding: 6em 0;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .left-container {
+        max-width: 400px;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+    }
+
+    .slogan {
+        text-align: left;
+        color: black;
+        font-weight: 900;
+        font-size: 24px;
+    }
+
+    .logo {
+        /* max-height: 80px; */
+        display: flex;
+        /* flex-direction: column; */
+        justify-content: flex-start;
+    }
+
+    .nav-logo {
+        max-height: 100px;
+    }
+
+    .badges {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 20px;
+    }
+
+    .store-badge {
+        width: 200px;
+    }
+
+    .right-container {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .right-container a {
+        text-align: left;
+        color: black;
+        margin-bottom: 20px;
+    }
+
+    .footer {
+        width: 100%;
+        max-width: 980px;
+        background-color: #1f1f1f;
+        height: 90px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 30px;
+    }
+
+    .footer-logo {
+        height: 70px;
+    }
+
+    .footer div {
+    }
+</style>
---
+++ src/sections/sample1/about-section.svelte
@@ -0,0 +1,195 @@
+<script>
+    import { onMount } from "svelte";
+    import { reveal } from "svelte-reveal";
+
+    let characterX = 0;
+    let characterY = 0;
+    const moveRatio = 0.01;
+    let isTablet = false;
+
+    if (!isTablet) {
+        function handleMouseMove(event) {
+            const deltaX = (event.clientX - window.innerWidth / 2) * moveRatio;
+            const deltaY = (event.clientY - window.innerHeight / 2) * moveRatio;
+
+            characterX = -deltaX;
+            characterY = -deltaY;
+        }
+
+        onMount(() => {
+            window.addEventListener("mousemove", handleMouseMove);
+        });
+    }
+
+    function checkScreenWidth() {
+        isTablet = window.innerWidth < 980;
+    }
+
+    window.addEventListener("resize", checkScreenWidth);
+
+    checkScreenWidth();
+</script>
+
+{#if !isTablet}
+    <section id="about" class="bg-white">
+        <div class="container">
+            <div class="about-element">
+                <div class="element-title">
+                    <h2>Lorem ipsum dolor</h2>
+                </div>
+                <img
+                    class="about-image"
+                    src="images/about-image.png"
+                    alt="home-horizontal"
+                />
+                <div class="element-script">
+                    Lorem ipsum dolor sit amet, adipiscing elit consectetur
+                </div>
+            </div>
+            <div class="about-element">
+                <div class="element-title">
+                    <h2>Lorem ipsum dolor</h2>
+                </div>
+                <img
+                    class="about-image"
+                    src="images/about-image.png"
+                    alt="home-horizontal"
+                />
+                <div class="element-script">
+                    Lorem ipsum dolor sit amet, adipiscing elit consectetur
+                </div>
+            </div>
+            <div class="about-element">
+                <div class="element-title">
+                    <h2>Lorem ipsum dolor</h2>
+                </div>
+                <img
+                    class="about-image"
+                    src="images/about-image.png"
+                    alt="home-horizontal"
+                />
+                <div class="element-script">
+                    Lorem ipsum dolor sit amet, adipiscing elit consectetur
+                </div>
+            </div>
+        </div>
+        <!-- <div id="about" class="desktop-section-container" /> -->
+    </section>
+{/if}
+{#if isTablet}
+    <section id="about" class="bg-white">
+        <div class="container-mb">
+            <div class="about-element-mb">
+                <div class="element-title-mb">
+                    <h2>Lorem ipsum dolor</h2>
+                </div>
+                <img
+                    class="about-image-mb"
+                    src="images/about-image.png"
+                    alt="home-horizontal"
+                />
+                <div class="element-script-mb">
+                    Lorem ipsum dolor sit amet, adipiscing elit consectetur
+                </div>
+            </div>
+            <div class="about-element-mb">
+                <div class="element-title-mb">
+                    <h2>Lorem ipsum dolor</h2>
+                </div>
+                <img
+                    class="about-image-mb"
+                    src="images/about-image.png"
+                    alt="home-horizontal"
+                />
+                <div class="element-script-mb">
+                    Lorem ipsum dolor sit amet, adipiscing elit consectetur
+                </div>
+            </div>
+            <div class="about-element-mb">
+                <div class="element-title-mb">
+                    <h2>Lorem ipsum dolor</h2>
+                </div>
+                <img
+                    class="about-image-mb"
+                    src="images/about-image.png"
+                    alt="home-horizontal"
+                />
+                <div class="element-script-mb">
+                    Lorem ipsum dolor sit amet, adipiscing elit consectetur
+                </div>
+            </div>
+        </div>
+        <!-- <div id="about" class="desktop-section-container" /> -->
+    </section>
+{/if}
+
+<style>
+    .bg-white {
+        background-color: white;
+    }
+    .container {
+        /* padding-top: 70px; */
+        width: 100%;
+        /* height: calc(100vh - 70px - 6em); */
+        max-width: 1080px;
+        margin: 0 auto;
+        padding: calc(70px + 6em) 0 6em 0;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .about-element {
+        max-width: 300px;
+        /* display: flex; */
+        color: black;
+    }
+
+    .about-element img {
+        width: 300px;
+    }
+
+    .element-title {
+        background: linear-gradient(transparent 70%, #54caff 0%);
+    }
+
+    .element-script {
+        text-align: left;
+    }
+
+    /* for mobile / tablet */
+
+    .container-mb {
+        /* padding-top: 70px; */
+        width: 100%;
+        /* height: calc(100vh - 70px - 6em); */
+        max-width: 1080px;
+        margin: 0 auto;
+        padding: 70px 0 70px 0;
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        gap: 30px;
+    }
+
+    .about-element-mb {
+        max-width: 300px;
+        /* display: flex; */
+        color: black;
+        flex: 1 0 calc(100%); /* flex-grow, flex-shrink, flex-basis */
+        /* margin: 5px; */
+    }
+
+    .about-element-mb img {
+        width: 300px;
+    }
+
+    .element-title-mb {
+        background: linear-gradient(transparent 70%, #54caff 0%);
+    }
+
+    .element-script-mb {
+        text-align: left;
+    }
+</style>
