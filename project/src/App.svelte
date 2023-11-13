329f7a58e9d62c068c005d04041d66fbf751f767
kimchan122 <chanchankim122@gmail.com>
2023-11-13 20:38:32 +0700

    코드 정리

--- project/src/App.svelte
+++ project/src/App.svelte
@@ -1,14 +1,5 @@
 <script>
   import PageSample1 from "./pages/page.svelte";
-  // import { onMount } from "svelte";
-
-  // function scrollToTop() {
-  //   window.scrollTo(0, 0);
-  // }
-
-  // onMount(() => {
-  //   scrollToTop();
-  // });
 </script>
 
 <PageSample1 />
