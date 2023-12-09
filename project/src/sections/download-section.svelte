b2daed3fb05096ba14dac8ef3705eeed07abf706
kimchan122 <chanchankim122@gmail.com>
2023-12-10 01:02:53 +0900

    download styles

--- project/src/sections/download-section.svelte
+++ project/src/sections/download-section.svelte
@@ -53,308 +53,66 @@
     checkScreenWidth();
 </script>
 
-{#if !isTablet}
-    <section
-        id="download"
-        class="bg-white"
-        use:inview={options}
-        on:inview_change={(event) => {
-            const { inView } = event.detail;
-            isInView = inView;
-        }}
-    >
-        <div class="container">
-            <div class="role">
-                <h1 use:reveal={{ transition: "fade", delay: 0 }}>DOWNLOAD</h1>
-                <div class="role-box">
-                    {#each titleArray as title, i}
-                        <div
-                            use:reveal={{ transition: "fade", delay: i * 100 }}
-                        >
-                            <div class="role-box-image-container">
-                                <img
-                                    class="role-box-image"
-                                    src={imageArray[i]}
-                                    alt="role"
-                                />
-                            </div>
-                            <h2>{title}</h2>
-                            <p>{scriptArray[i]}</p>
-                        </div>
-                    {/each}
-                </div>
-            </div>
-            <div
-                class="download-box"
-                use:reveal={{ transition: "fade", delay: 400 }}
-            >
-                <img
-                    class="app-logo"
-                    src="images/app-logo.png"
-                    alt="app-logo"
-                    use:reveal={{ transition: "fade", delay: 500 }}
-                />
-                <div>
-                    <h2 use:reveal={{ transition: "fade", delay: 600 }}>
-                        지금 다운로드하세요!
-                    </h2>
-                    <div
-                        class="badges"
-                        use:reveal={{ transition: "fade", delay: 700 }}
-                    >
+<section
+    id="download"
+    class="bg-white"
+    use:inview={options}
+    on:inview_change={(event) => {
+        const { inView } = event.detail;
+        isInView = inView;
+    }}
+>
+    <div class="container">
+        <div class="role">
+            <h1 use:reveal={{ transition: "fade", delay: 0 }}>DOWNLOAD</h1>
+            <div class="role-box">
+                {#each titleArray as title, i}
+                    <div use:reveal={{ transition: "fade", delay: i * 100 }}>
                         <img
-                            class="store-badge"
-                            src="images/google-play-badge.png"
-                            alt="download-google-play"
+                            class="role-box-image"
+                            src={imageArray[i]}
+                            alt="role"
                         />
-                        <img
-                            class="store-badge"
-                            src="images/Download_on_the_App_Store_Badge_KR_RGB_blk_100317.svg"
-                            alt="download-app-store"
-                        />
+                        <h2>{title}</h2>
+                        <p>{scriptArray[i]}</p>
                     </div>
+                {/each}
+            </div>
+        </div>
+        <div
+            class="download-box"
+            use:reveal={{ transition: "fade", delay: 400 }}
+        >
+            <img
+                class="app-logo"
+                src="images/app-logo.png"
+                alt="app-logo"
+                use:reveal={{ transition: "fade", delay: 500 }}
+            />
+            <div>
+                <h2 use:reveal={{ transition: "fade", delay: 600 }}>
+                    지금 다운로드하세요!
+                </h2>
+                <div
+                    class="badges"
+                    use:reveal={{ transition: "fade", delay: 700 }}
+                >
+                    <img
+                        class="store-badge"
+                        src="images/google-play-badge.png"
+                        alt="download-google-play"
+                    />
+                    <img
+                        class="store-badge"
+                        src="images/Download_on_the_App_Store_Badge_KR_RGB_blk_100317.svg"
+                        alt="download-app-store"
+                    />
                 </div>
             </div>
         </div>
-        <!-- <div id="about" class="desktop-section-container" /> -->
-    </section>
-{/if}
-{#if isTablet}
-    <section
-        id="download"
-        class="bg-white-mb"
-        use:inview={options}
-        on:inview_change={(event) => {
-            const { inView } = event.detail;
-            isInView = inView;
-        }}
-    >
-        <div class="container-mb">
-            <div class="role-mb">
-                <h1 use:reveal={{ transition: "fade", delay: 0 }}>DOWNLOAD</h1>
-                <div class="role-box-mb">
-                    {#each titleArray as title, i}
-                        <div
-                            use:reveal={{ transition: "fade", delay: i * 100 }}
-                        >
-                            <img
-                                class="role-box-image-mb"
-                                src={imageArray[i]}
-                                alt="role"
-                            />
-                            <h2>{title}</h2>
-                            <p>{scriptArray[i]}</p>
-                        </div>
-                    {/each}
-                </div>
-            </div>
-            <div
-                class="download-box-mb"
-                use:reveal={{ transition: "fade", delay: 400 }}
-            >
-                <img
-                    class="app-logo-mb"
-                    src="images/app-logo.png"
-                    alt="app-logo"
-                    use:reveal={{ transition: "fade", delay: 500 }}
-                />
-                <div>
-                    <h2 use:reveal={{ transition: "fade", delay: 600 }}>
-                        지금 다운로드하세요!
-                    </h2>
-                    <div
-                        class="badges-mb"
-                        use:reveal={{ transition: "fade", delay: 700 }}
-                    >
-                        <img
-                            class="store-badge-mb"
-                            src="images/google-play-badge.png"
-                            alt="download-google-play"
-                        />
-                        <img
-                            class="store-badge-mb"
-                            src="images/Download_on_the_App_Store_Badge_KR_RGB_blk_100317.svg"
-                            alt="download-app-store"
-                        />
-                    </div>
-                </div>
-            </div>
-            <!-- <div id="about" class="desktop-section-container" /> -->
-        </div>
-    </section>
-{/if}
+    </div>
+</section>
 
 <style>
-    /* .bg-gray {
-        background-color: #eeeeee;
-        margin: 0 auto;
-    } */
-
-    .bg-white {
-        background-color: white;
-        height: 0 auto;
-    }
-
-    .container {
-        width: 100%;
-        max-width: 1080px;
-        margin: 0 auto;
-        padding: 70px 20px 70px 20px;
-        display: flex;
-        flex-direction: column;
-        color: black;
-        box-sizing: border-box;
-    }
-    .role {
-        padding-bottom: 6em;
-    }
-    .role-box {
-        display: flex;
-        justify-content: space-between;
-    }
-    .role-box-image-container {
-        width: 200px;
-        height: 200px;
-        overflow: hidden;
-    }
-    .role-box-image {
-        width: 200px;
-        height: 200px;
-        object-fit: cover;
-        overflow: hidden;
-        transform-origin: center center;
-        transition: transform 0.3s ease-in-out;
-    }
-    .role-box-image:hover {
-        transform: scale(1.2);
-    }
-    .role-box div {
-        max-width: 200px;
-    }
-
-    .role-box div h2 {
-        font-size: 20px;
-    }
-    .download-box {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        background-color: white;
-        /* background-color: #32c7ff; */
-        border: 3px solid #ffdaa3;
-        border-radius: 30px;
-        /* margin-bottom: 6em; */
-    }
-
-    .app-logo {
-        width: 200px;
-        padding: 20px;
-    }
-
-    .badges {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        gap: 20px;
-    }
-
-    .store-badge {
-        width: 200px;
-    }
-
-    /* for mobile/tablet */
-
-    .bg-white-mb {
-        background-color: white;
-        height: 0 auto;
-    }
-    /* .bg-gray {
-        background-color: #eeeeee;
-        margin: 0 auto;
-    } */
-    .container-mb {
-        /* padding-top: 70px; */
-        width: 100%;
-        /* height: calc(100vh - 70px); */
-        /* max-width: 1080px; */
-        margin: 0 auto;
-        display: flex;
-        flex-direction: column;
-        color: black;
-        padding: 70px 30px;
-        box-sizing: border-box;
-    }
-    .role-mb {
-        padding-bottom: 6em;
-    }
-    .role-box-mb {
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-around;
-    }
-
-    .role-box-mb div {
-        flex: 1 0 calc(50% - 10px); /* flex-grow, flex-shrink, flex-basis */
-        margin: 5px;
-    }
-
-    .role-box-mb div h2 {
-        font-size: 20px;
-    }
-
-    .role-box-image-mb {
-        width: 200px;
-        height: 200px;
-        object-fit: cover;
-    }
-    .download-box-mb {
-        display: flex;
-        /* flex-direction: column; */
-        justify-content: center;
-        align-items: center;
-        /* background-color: white; */
-        background-color: white;
-        border: 3px solid #ffdaa3;
-        border-radius: 30px;
-        /* margin-bottom: 6em; */
-        padding: 30px 30px;
-        flex-wrap: wrap;
-    }
-
-    @media (max-width: 800px) {
-        .download-box-mb {
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            /* background-color: white; */
-            background-color: #ffdaa3;
-            border: 3px solid #ffdaa3;
-            border-radius: 30px;
-            /* margin-bottom: 6em; */
-            padding: 30px 30px;
-            flex-wrap: wrap;
-        }
-    }
-
-    .download-box-mb div h2 {
-        margin-bottom: 20px;
-    }
-
-    .app-logo-mb {
-        width: 200px;
-        padding: 20px;
-    }
-
-    .badges-mb {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-wrap: wrap;
-        gap: 10px;
-    }
-
-    .store-badge-mb {
-        width: 200px;
-    }
+    @import "../styles/downloadSectionStyles.scss";
 </style>
--- project/src/sections/news-section.svelte
+++ project/src/sections/news-section.svelte
@@ -63,54 +63,55 @@
     ];
 </script>
 
-{#if !isTablet}
-    <section
-        id="news"
-        class="bg-white"
-        use:inview={options}
-        on:inview_change={(event) => {
-            const { inView } = event.detail;
-            isInView = inView;
-        }}
-    >
-        <div class="container">
-            <div class="role">
-                <h1 use:reveal={{ transition: "fade", delay: 0 }}>NEWS</h1>
+<!-- {#if !isTablet} -->
+<section
+    id="news"
+    class="bg-white"
+    use:inview={options}
+    on:inview_change={(event) => {
+        const { inView } = event.detail;
+        isInView = inView;
+    }}
+>
+    <div class="container">
+        <div class="role">
+            <h1 use:reveal={{ transition: "fade", delay: 0 }}>NEWS</h1>
 
-                <div use:reveal={{ transition: "fade", delay: 100 }}>
-                    <Carousel
-                        particlesToShow={3}
-                        particlesToScroll={1}
-                        autoplay
-                        autoplayDuration={4000}
-                        dots={false}
-                    >
-                        {#each newsItems as item (item.image)}
-                            <div class="carousel-box">
-                                <div class="carousel-element">
-                                    <img
-                                        class="news-image"
-                                        src={item.image}
-                                        alt=""
-                                    />
-                                    <div class="news-text">
-                                        <h3 class="news-title">
-                                            {item.title}
-                                        </h3>
-                                        <p class="news-script">
-                                            {item.description}
-                                        </p>
-                                    </div>
+            <div use:reveal={{ transition: "fade", delay: 100 }}>
+                <Carousel
+                    particlesToShow={3}
+                    particlesToScroll={1}
+                    autoplay
+                    autoplayDuration={4000}
+                    dots={false}
+                >
+                    {#each newsItems as item (item.image)}
+                        <div class="carousel-box">
+                            <div class="carousel-element">
+                                <img
+                                    class="news-image"
+                                    src={item.image}
+                                    alt=""
+                                />
+                                <div class="news-text">
+                                    <h3 class="news-title">
+                                        {item.title}
+                                    </h3>
+                                    <p class="news-script">
+                                        {item.description}
+                                    </p>
                                 </div>
                             </div>
-                        {/each}
-                    </Carousel>
-                </div>
+                        </div>
+                    {/each}
+                </Carousel>
             </div>
         </div>
-    </section>
-{/if}
-{#if isTablet}
+    </div>
+</section>
+
+<!-- {/if} -->
+<!-- {#if isTablet}
     <section
         id="news"
         class="bg-white"
@@ -155,224 +156,8 @@
             </div>
         </div>
     </section>
-{/if}
+{/if} -->
 
 <style>
-    .bg-white {
-        background-color: white;
-    }
-    .container {
-        width: 100%;
-        max-width: 1080px;
-        margin: 0 auto;
-        padding: 70px 20px 70px 20px;
-        display: flex;
-        flex-direction: column;
-        color: black;
-        box-sizing: border-box;
-    }
-    .role {
-        padding-bottom: 6em;
-    }
-    /* .role-box {
-        display: flex;
-        justify-content: space-between;
-    }
-    .role-box-image {
-        width: 200px;
-        height: 200px;
-        object-fit: cover;
-    }
-
-    .svelte-carousel .dots .dot.active {
-        background-color: red;
-    }
-
-    .desktop-section-container {
-        width: 100%;
-        height: calc(100vh);
-        padding-top: 70px;
-        max-width: 980px;
-        margin: 0 auto;
-        box-sizing: border-box;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        color: black;
-        flex-wrap: wrap;
-    }
-
-    .download-box {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        background-color: white;
-        border-radius: 30px;
-        margin-bottom: 6em;
-    }
-
-    .app-logo {
-        width: 200px;
-        padding: 20px;
-    }
-
-    .badges {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        gap: 20px;
-    }
-
-    .store-badge {
-        width: 200px;
-    } */
-
-    .carousel-box {
-        width: 250px;
-        max-width: 250px;
-        height: 350px;
-        max-height: 350px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        /* background-color: white; */
-        /* margin: 20px; */
-        /* box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.5); */
-    }
-    .carousel-element {
-        width: 270px;
-        height: 300px;
-        padding: 10px;
-        background-color: white;
-        /* margin: 20px; */
-        border-radius: 10px;
-        box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.1);
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        /* justify-content: space-around; */
-        margin-left: 10px;
-        margin-right: 10px;
-    }
-
-    .news-image {
-        width: 250px;
-        height: 120px;
-        object-fit: cover;
-    }
-
-    .news-text {
-        display: flex;
-        flex-direction: column;
-    }
-
-    .news-title {
-        width: 250px;
-        font-weight: 500;
-        /* line-height: 30px; */
-        /* margin: 0; */
-        display: -webkit-box;
-        -webkit-box-orient: vertical;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        -webkit-line-clamp: 2;
-        text-align: left;
-        /* max-height: 2em; */
-    }
-
-    .news-script {
-        width: 250px;
-        margin: 0;
-        display: -webkit-box;
-        -webkit-box-orient: vertical;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        -webkit-line-clamp: 3;
-        text-align: left;
-        max-height: 4.5em;
-    }
-
-    /* for mobile/tablet */
-
-    .container-mb {
-        width: 100%;
-        height: 100vh;
-        /* max-width: 1080px; */
-        margin: 0 auto;
-        /* padding: 70px 0 70px 0; */
-        display: flex;
-        flex-direction: column;
-        color: black;
-        justify-content: center;
-        padding: 70px 30px 70px 30px;
-        box-sizing: border-box;
-    }
-    .role-mb {
-        padding-bottom: 6em;
-    }
-
-    .carousel-box-mb {
-        width: 100%;
-        max-width: 250px;
-        height: 350px;
-        max-height: 350px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        /* background-color: white; */
-        /* margin: 20px; */
-        /* box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.5); */
-    }
-    .carousel-element-mb {
-        width: 270px;
-        height: 300px;
-        padding: 10px;
-        background-color: white;
-        /* margin: 20px; */
-        border-radius: 10px;
-        box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.1);
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        /* justify-content: space-around; */
-        margin-left: 10px;
-        margin-right: 10px;
-    }
-
-    .news-image-mb {
-        width: 250px;
-        height: 120px;
-        object-fit: cover;
-    }
-
-    .news-text-mb {
-        display: flex;
-        flex-direction: column;
-    }
-
-    .news-title-mb {
-        width: 250px;
-        font-weight: 500;
-        /* line-height: 30px; */
-        /* margin: 0; */
-        display: -webkit-box;
-        -webkit-box-orient: vertical;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        -webkit-line-clamp: 2;
-        text-align: left;
-        /* max-height: 2em; */
-    }
-
-    .news-script-mb {
-        width: 250px;
-        margin: 0;
-        display: -webkit-box;
-        -webkit-box-orient: vertical;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        -webkit-line-clamp: 3;
-        text-align: left;
-        max-height: 4.5em;
-    }
+    @import "../styles/newsSectionStyles.scss";
 </style>
